efe2ed49f8ef1f6bc78a2c894d21f318131a9a0e
Anthony Anderson <fnanderson93@gmail.com>
2023-11-09 11:52:11 -0500

    revert: :rewind: remove reserved variable changes

--- plugins/inventory/nb_inventory.py
+++ plugins/inventory/nb_inventory.py
@@ -119,7 +119,7 @@ DOCUMENTATION = """
             default: False
             type: boolean
             version_added: "3.5.0"
-        netbox_services:
+        services:
             description:
                 - If True, it adds the device or virtual machine services information in host vars.
             default: True
@@ -594,10 +594,10 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
                     }
                 )
 
-        if self.netbox_services:
+        if self.services:
             extractors.update(
                 {
-                    "netbox_services": self.extract_services,
+                    "services": self.extract_services,
                 }
             )
 
@@ -1443,7 +1443,7 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
         if self.prefixes:
             lookups.append(self.refresh_prefixes)
 
-        if self.netbox_services:
+        if self.services:
             lookups.append(self.refresh_services)
 
         if self.racks:
@@ -1696,7 +1696,7 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
                 return None
 
         # Special case. Extract name from service, which is a hash.
-        if grouping == "netbox_services":
+        if grouping == "services":
             group = group["name"]
             grouping = "service"
 
@@ -2036,7 +2036,7 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
         self.flatten_custom_fields = self.get_option("flatten_custom_fields")
         self.plurals = self.get_option("plurals")
         self.interfaces = self.get_option("interfaces")
-        self.netbox_services = self.get_option("services")
+        self.services = self.get_option("services")
         self.site_data = self.get_option("site_data")
         self.prefixes = self.get_option("prefixes")
         self.fetch_all = self.get_option("fetch_all")
