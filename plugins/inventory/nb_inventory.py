7833d4fc8a1dfff676e2489f68202a8d1dc078a0
Anthony Anderson <fnanderson93@gmail.com>
2023-11-09 12:34:33 -0500

    fix: :bug: broke it again

--- plugins/inventory/nb_inventory.py
+++ plugins/inventory/nb_inventory.py
@@ -119,7 +119,7 @@ DOCUMENTATION = """
             default: False
             type: boolean
             version_added: "3.5.0"
-        services:
+        netbox_services:
             description:
                 - If True, it adds the device or virtual machine services information in host vars.
             default: True
@@ -168,7 +168,7 @@ DOCUMENTATION = """
                 - cluster_type
                 - cluster_group
                 - is_virtual
-                - services
+                - netbox_services
                 - status
                 - time_zone
                 - utc_offset
@@ -1696,7 +1696,7 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
                 return None
 
         # Special case. Extract name from service, which is a hash.
-        if grouping == "services":
+        if grouping == "netbox_services":
             group = group["name"]
             grouping = "service"
 
